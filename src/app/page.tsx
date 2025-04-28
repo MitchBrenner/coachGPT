fbe0a6f669c56a3359ca6a9bfc8a06105ef0800b
Mitchell Brenner <mitchbrenner8@gmail.com>
2025-04-28 00:59:07 -0600

    Minor mobile UI fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ import React from "react";
 function page() {
   return (
     <div className="flex flex-col min-h-screen text-foreground overflow-hidden">
-      <section className="relative z-10 py-24 flex-grow">
+      <section className="relative z-10 pt-0 sm:py-24 flex-grow">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 lg:grid-cols-12 items-center relative">
             {/* corner decoration */}
-            <div className="absolute -top-10 left-0 w-40 h-40 border-l-2 border-t-2" />
+            <div className="hidden sm:block absolute -top-10 left-0 w-40 h-40 border-l-2 border-t-2" />
 
             {/* Left side content */}
             <div className="lg:col-span-7 space-y-8 relative p-4 lg:p-0">
